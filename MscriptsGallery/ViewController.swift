eaa5f7ae32df022bb476c5fb58b14bb94182a251
Bharath.Vankireddy <bharathkumarreddy.vankireddy@boeing.com>
2020-02-23 14:41:58 +0530

    Initial Commit

---
+++ MscriptsGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MscriptsGallery
+//
+//  Created by A6049116 on 2/23/20.
+//  Copyright © 2020 Mscripts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
